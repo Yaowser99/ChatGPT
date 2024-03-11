e2b91990fd19b7e68c23756ac7fdf9e187212b2d
Yaowser <149530866+Yaowser99@users.noreply.github.com>
2024-03-11 16:46:57 -0400

    Update openai_api.py

--- openai_api.py
+++ openai_api.py
@@ -684,6 +684,43 @@ print(response)
 
 
 ## Chain of thought and self consistency promption
+# Set your API key
+client = OpenAI(api_key="")
+
+# Create the chain-of-thought prompt
+prompt = """Q: Your friend is 20 year old. His father is now double of your friend's age. What is your friend's father's age in 10 years? A: Let's think step by step"""
+
+response = get_response(prompt)
+print(response)
+
+
+# Define the example 
+example = """Q: Sum the even numbers in the following set: {9, 10, 13, 4, 2}.
+             A: Even numbers: {10,4,2}. Adding them: 10+4+2=16"""
+
+# Define the question
+question = """Q: Sum the even numbers in the following set: {15, 13, 82, 7, 14}. 
+              A:"""
+
+# Create the final prompt
+prompt = example+question
+response = get_response(prompt)
+print(response)
+
+# Create the self_consistency instruction
+self_consistency_instruction = "Imagine three completely independent experts who reason differently are answering this questions. The final answer is obtained by majority vote. The question is: "
+
+# Create the problem to solve
+problem_to_solve = "If you own a store that sells laptops and mobile phones. You start your day with 50 devices in the store, out of which 60% are mobile phones. Throughout the day, three clients visited the store, each of them bought one mobile phone, and one of them bought additionally a laptop. Also, you added to your collection 10 laptops and 5 mobile phones. How many laptops and mobile phones do you have by the end of the day?"
+
+# Create the final prompt
+prompt = self_consistency_instruction + problem_to_solve
+
+response = get_response(prompt)
+print(response)
+
+
+# Iterative prompt engineering and refindment
 
 
 
